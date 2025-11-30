45a36740b76737bf5dc7a8b4f13070a81e3cbaa4
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-11-30 15:47:00 +0100

    chore: Updated tests to match refactored features

--- src/utils/tests/e2e/overview-h1.spec.ts
+++ src/utils/tests/e2e/overview-h1.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from "@playwright/test";
 import { bypassFadeOut } from "./test-setup";
 
 test("Overview page h1 text", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("/overview");
   await bypassFadeOut(page);
 
   const h1 = page.locator("#page-title");
-  await expect(h1).toHaveText("Bits Auction-house");
+  await expect(h1).toHaveText("Bits Auctions");
 });
