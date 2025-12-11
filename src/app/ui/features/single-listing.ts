b7364cc374488d6355a9fd040490a2d18f4aa72f
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-12-11 08:24:04 +0100

    Authenticated user can place bids on active listings feat: Place bid button on single listing page feat: Usermessage confirmation of successfully placed bid with credit amount todo: Wire placed bid with bid amount and user credit totals

--- src/app/ui/features/single-listing.ts
+++ src/app/ui/features/single-listing.ts
@@ -2,6 +2,9 @@ import type { Listing } from "@/services/types/listing";
 import type { Profile } from "@/services/types/profile";
 import renderContent from "../render-content";
 import { popUpModal } from "@/app/components/modal/modal";
+import { getStatusBadge } from "@/app/components/listings/helpers/get-status-badge";
+import { userMessage } from "@/app/ui/utils/user-messages";
+import { isAuthenticated } from "@/utils/config/constants";
 
 const SingleListing = async (listing: Listing) => {
   const container = document.getElementById("content");
@@ -22,7 +25,7 @@ const SingleListing = async (listing: Listing) => {
     img.src = media.url;
     img.alt = listing.title || "Listing image";
     img.className =
-      "flex-shrink-0 flex items-center justify-center w-58 h-58 md:w-80 md:h-80 rounded-sm object-cover";
+      "flex-shrink-0 flex items-center justify-center w-58 h-58 md:w-80 md:h-80 rounded-sm object-cover cursor-pointer";
     mediaImages.appendChild(img);
 
     img.addEventListener("click", () => {
@@ -31,29 +34,71 @@ const SingleListing = async (listing: Listing) => {
   });
 
   const detailsSection = document.createElement("div");
-  detailsSection.className = "px-4";
+  detailsSection.className =
+    "px-4 mb-6 flex flex-row items-start justify-between w-full";
+
+  const textSection = document.createElement("div");
+  textSection.className = "w-[60%]";
 
   const title = document.createElement("h1");
   title.className = "text-3xl font-bold mb-2";
   title.textContent = listing.title || "Untitled listing";
 
   const description = document.createElement("p");
-  description.className = "text-gray-200 text-md mb-4";
+  description.className = "text-black dark:text-gray-200 text-md mb-4";
   description.textContent = listing.description || "No description";
 
   const seller = document.createElement("p");
   seller.className =
-    "w-40 text-gray-400 text-sm mb-4 hover:underline hover:text[var(--accent-strong)] cursor-pointer";
+    "w-40 text-gray-600 dark:text-gray-300 text-sm mb-4 hover:underline hover:text-[var(--accent-strong)] cursor-pointer";
   seller.textContent = (listing.seller as Profile)?.name || "Unknown seller";
+
+  const actionCenter = document.createElement("div");
+  actionCenter.className = "flex flex-row items-center justify-end gap-4";
+
+  const status = document.createElement("p");
+  status.className = "text-sm";
+  status.appendChild(
+    getStatusBadge(listing.endsAt > new Date() ? "active" : "ended")
+  );
+
+  const bids = document.createElement("p");
+  bids.className = "text-sm";
+  bids.innerHTML = listing._count.bids
+    ? `${listing._count.bids} bids`
+    : "0 bids";
+
+  const placeBidButton = document.createElement("button");
+  placeBidButton.className = "btn btn-primary";
+  placeBidButton.textContent = "Place Bid";
+
+  if (new Date(listing.endsAt) > new Date() && isAuthenticated) {
+    actionCenter.appendChild(placeBidButton);
+  }
+
+  let usedCredits; // profile.credits - bid amount = creditsLeft
+  placeBidButton.addEventListener("click", () => {
+    // implement bidding logic --> set actual amount, e.g. 200 credits
+    // + confirmation on bid placed
+    userMessage(
+      "success",
+      `You placed a bid of ${usedCredits || "200 credits"}!`
+    );
+  });
 
   seller.addEventListener("click", () => {
     window.location.pathname = `/bits-auctions/profile/${listing.seller.name}`;
     renderContent();
   });
 
-  detailsSection.appendChild(title);
-  detailsSection.appendChild(description);
-  detailsSection.appendChild(seller);
+  textSection.appendChild(title);
+  textSection.appendChild(description);
+  textSection.appendChild(seller);
+  detailsSection.appendChild(textSection);
+
+  actionCenter.appendChild(status);
+  actionCenter.appendChild(bids);
+  detailsSection.appendChild(actionCenter);
 
   mediaCarousel.appendChild(mediaImages);
   container.appendChild(mediaCarousel);
