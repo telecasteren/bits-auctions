bff019b68166adc5e15405456055a0ad67f3b2bd
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-02-01 18:39:09 +0100

    typescript compiler added to ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,14 +10,13 @@ export default defineConfig({
   fullyParallel: false,
 
   // Fail the build on CI if you accidentally left test.only in the source code.
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
 
   // Retry on CI only.
   retries: process.env.CI ? 2 : 0,
 
   // Opt out of parallel tests on CI.
-  // workers: process.env.CI ? 1 : undefined,
-  workers: 1,
+  workers: process.env.CI ? 1 : undefined,
 
   // Reporter to use
   reporter: "html",
