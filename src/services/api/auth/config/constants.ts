24ab65c7196cd4434afc0a1db95f66206ce58ced
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-02-05 08:32:38 +0100

    Add safe defaults for API endpoints

--- src/services/api/auth/config/constants.ts
+++ src/services/api/auth/config/constants.ts
@@ -1,6 +1,7 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const LISTINGS = import.meta.env.VITE_API_LISTINGS;
+export const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://v2.api.noroff.dev";
+export const LISTINGS = import.meta.env.VITE_API_LISTINGS || "/auction/listing";
 export const PER_PAGE = import.meta.env.VITE_LISTINGS_PER_PAGE;
-export const LOGIN = import.meta.env.VITE_API_LOGIN;
-export const REGISTER = import.meta.env.VITE_API_REGISTER;
-export const USERS = import.meta.env.VITE_API_USERS;
+export const LOGIN = import.meta.env.VITE_API_LOGIN || "/login";
+export const REGISTER = import.meta.env.VITE_API_REGISTER || "/register";
+export const USERS = import.meta.env.VITE_API_USERS || "/auction/profiles";
